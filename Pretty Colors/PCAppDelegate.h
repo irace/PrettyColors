bc9c5b6dc1bea18fa4e09fac4b591394dcf44283
Bryan Irace <bryan.irace@gmail.com>
2013-08-11 14:44:11 -0400

    Initial Commit

---
+++ Pretty Colors/PCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PCAppDelegate.h
+//  Pretty Colors
+//
+//  Created by Bryan Irace on 8/11/13.
+//  Copyright (c) 2013 Bryan Irace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pretty Colors/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pretty Colors
+//
+//  Created by Bryan Irace on 8/11/13.
+//  Copyright (c) 2013 Bryan Irace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PCAppDelegate class]));
+    }
+}
